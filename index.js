69d49af961b2dd4e42ac42263c09015bef450187
Arnisha233 <75923555+Arnisha233@users.noreply.github.com>
2024-02-19 03:34:40 +0600

    added style and js function

--- index.js
+++ index.js
@@ -1,55 +1,180 @@
 
 
-const allBtn = document.getElementsByClassName("add-btn");
+// const allBtn = document.getElementsByClassName("add-btn");
 
-let count = 0;
 
-for(btn of allBtn){
+// let count = 0;
 
-    btn.addEventListener("click", function(e){
+// for(btn of allBtn){
+
+//     btn.addEventListener("click", function(e){
         
-        count = count + 1;
+//         count = count + 1;
 
-        const seat = document.getElementById("seat").innerText;
+//         const seat = document.getElementById("seat").innerText;
         
-        document.getElementById("seat").innerText = count;
+//         document.getElementById("seat").innerText = count;
 
 
-        const button = e.target.innerText;
+//         const button = e.target.innerText;
         
-        
-      
-
         
 
         
-        const name = e.target.parentNode.parentNode.parentNode.parentNode.childNodes[15].childNodes[5].childNodes[3].childNodes[3].childNodes[1].childNodes[1].childNodes[7];
+//         const name = e.target.parentNode.parentNode.parentNode.parentNode.childNodes[15].childNodes[5].childNodes[3].childNodes[3].childNodes[1].childNodes[1].childNodes[7];
        
 
     
 
-        const select = document.getElementById("moha");
+//         const select = document.getElementById("moha");
 
-        const selectTwo = document.getElementById("mohaTwo");
-
-        
-        
-        const div = document.createElement("div");
-    div.innerText = button;
-
-    select.appendChild(div);
+//         const selectTwo = document.getElementById("mohaTwo");
 
         
         
-    const p = document.createElement("p");
-    p.innerText = button;
+//         const div = document.createElement("div");
+//     div.innerText = button;
 
-    selectTwo.appendChild(p);
+//     select.appendChild(div);
+
+        
+        
+//     const p = document.createElement("p");
+//     p.innerText = button;
+
+//     selectTwo.appendChild(p);
+
+
+
+//     });
+
+// }
 
 
 
 
-    });
+//  start js work
 
-}
 
+    const allBtn = document.getElementsByClassName("btn");
+
+    
+    let count = 0;
+
+    for(const btn of allBtn){
+        btn.addEventListener("click", function(e){
+
+            btn.style.backgroundColor = "#1DD100";
+            
+          count = count + 1;
+
+          
+
+          const ticketName = e.target.innerText;
+           
+
+
+            // 8 seat work 
+
+            // const seat = document.getElementById("green-sofa").innerText;
+            //  const seatConvert = parseInt(seat);
+            //  console.log(typeof seatConvert)
+
+            //  document.getElementById("green-sofa").innerText = ticketName - seatConvert;
+
+
+
+          const selectedContainer = document.getElementById("blank");
+          
+
+          const li = document.createElement("li");
+    
+          const p = document.createElement("p");
+          p.innerText = ticketName;
+         
+
+         
+
+
+            const p2 = document.createElement("p");
+          const text = document.createTextNode("Economoy ");
+          
+          
+          
+
+
+          const p3 = document.createElement("p");
+          const number = document.createTextNode("550");
+          
+          
+          
+          
+          li.appendChild(p);
+         li.appendChild(text);
+          li.appendChild(number);
+
+          
+      
+        //   const p2 = document.createElement("p2");
+        //   p2. innerText = Economoy;
+
+        //   const p3 = document.createElement("p2");
+        //   p3. innerText = 550;
+      
+      
+          selectedContainer.appendChild(li);
+        //   selectedContainer.appendChild(text)
+        //   selectedContainer.appendChild(number);
+        //   selectedContainer.appendChild(p3);
+          
+
+
+            
+
+            
+        // console.log(e.target.parentNode.parentNode.parentNode.childNodes[5].childNodes[3].childNodes[7].childNodes[0].childNodes[2]);
+
+        // const total = convertNumber.innerText
+        // parseFloat(total)
+        
+           
+
+
+        const totalCost = document.getElementById("total").innerText;
+        
+        const convertedTotalCost = parseInt(totalCost);
+        
+        
+
+        document.getElementById("total").innerText = parseInt(convertedTotalCost) + 550;
+        
+
+
+        // grand total
+
+
+        const grandTotalCost = document.getElementById("grand-total").innerText;
+
+        
+        const convertedGrandTotalCost = parseInt(grandTotalCost);
+
+
+        document.getElementById("grand-total").innerText = convertedGrandTotalCost + 550;
+
+
+
+        
+        const nextButton = document.getElementById("next").innerText;
+        
+        const section = document.getElementById("pop-pup");
+        section.classList.remove("hidden");
+
+
+
+
+
+         document.getElementById("cart-count").innerText = count;
+
+
+        })
+    }
